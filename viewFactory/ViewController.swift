1c22f6a5b76a53f5d9ce8b49febb49d1ccf25453
Pericles Narbal <pericles.oliveira@ciccr-ce.interno>
2022-04-27 16:32:01 -0300

    Initial Commit

---
+++ viewFactory/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  viewFactory
+//
+//  Created by Péricles Narbal on 27/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
