9487befee6bdbca41c8dbead4973a6b2ce8db00a
elaniacs <carensousaesilva@gmail.com>
2023-12-14 15:29:17 -0300

    TestFork

---
+++ viewFactory/TestFork.swift
@@ -0,0 +1,18 @@
+//
+//  TestFork.swift
+//  viewFactory
+//
+//  Created by Cáren on 14/12/23.
+//
+
+import SwiftUI
+
+struct TestFork: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TestFork()
+}
